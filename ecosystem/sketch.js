433874b60498947794f056ad6037f6b5ffc333ed
Manuel Rueda <manolorueda@gmail.com>
2017-02-01 02:39:17 -0500

    general updt

--- ecosystem/sketch.js
+++ ecosystem/sketch.js
@@ -1,6 +1,6 @@
 var entities = [];
 var foods = [];
-var total = 50;
+var total = 10;
 
 ////////////
 // SETUP  //
@@ -24,20 +24,24 @@ function setup() {
 ////////////
 
 function draw() {
-	background(200);
+	background(0);
 
 	for(var i=0; i < foods.length; i++) {
-		foods[i].show();
+		//foods[i].show();
 	}
 
 	for(var i=0; i < entities.length; i++){
 		entities[i].move();
 		entities[i].bounce();
-		//entities[i].bounce();
 		entities[i].show();
+		// seeking and fleeing
 		for(var j=0; j < entities.length; j++) {
 			if(entities[i] != entities[j]) {
-				entities[i].repel(entities[j]);
+				if(entities[i].sex == 0 && entities[j].sex == 1) {
+					entities[i].purse(entities[j]);
+				} else if(entities[i].sex == 1 && entities[j].sex == 0) {
+					entities[i].evade(entities[j]);
+				}
 			}
 		}
 	}
@@ -48,26 +52,54 @@ function draw() {
 ////////////
 
 function Entity(x_, y_, p_, a_, s_) {
+	// movement
 	this.acc    = createVector(0, 0);
 	this.vel    = createVector(0, 0);
 	this.loc    = createVector(x_, y_);
 	this.ploc   = [];
+	// noise
 	this.off    = random(999999);
+	this.ctr	  = random(1000);
+	// properties
 	this.prt    = p_;
 	this.acid   = a_;
-	this.rds    = map(this.acid, 0, 1, 5, 20);
+	this.rds    = map(this.acid, 0, 1, 5, 25);
 	this.mxvel  = map(this.acid * this.acid, 0, 1, 1, 0.1);
-	this.sex    = s_ > 0.8;
+	this.sex    = s_ > 0.7;
+	// female properties
+	if(this.sex) {
+		this.mxvel *= 1.2;
+		this.rds *= 0.5;
+	}
 
 	this.show = function() {
 		noStroke();
-		fill(220, 255, 220, 100);
-		if(this.sex) fill(255, 220 , 220, 100);
-		var nois = map(noise((frameCount + this.off)/100), 0, 1, -5, 5);
-		ellipse(this.ploc[0].x, this.ploc[0].y, this.rds + nois, this.rds + nois);
-		fill(0, 50, 0);
-		if(this.sex) fill(150, 0 , 0);
-		ellipse(this.loc.x, this.loc.y, 2 + nois, 2 + nois);
+		// male vs. female
+		push();
+		translate(this.loc.x, this.loc.y);
+		// outer shape
+		fill(200,100);
+		if(this.sex) fill(255, 100);
+		beginShape();
+		for (var a = 0; a < TWO_PI; a += PI/100) {
+			var nois = noise(cos(a) + 1, sin(a) + 1, this.ctr);
+			d = this.rds + map(nois, 0, 1, 0, 8);
+			vertex(d * cos(a), d * sin(a));
+		}
+		endShape();
+		// outer shape
+		fill(250, 50);
+		if(this.sex) fill(255, 50);
+		beginShape();
+		for (var a = 0; a < TWO_PI; a += PI/100) {
+			var nois = noise(cos(a) + 1, sin(a) + 1, this.ctr);
+			d = this.rds/3 + map(nois, 0, 1, 0, 3);
+			vertex(d * cos(a), d * sin(a));
+		}
+		endShape();
+		// reset
+		pop();
+		this.ctr += 0.05;
 	}
 
 	this.move = function() {
@@ -86,7 +118,38 @@ function Entity(x_, y_, p_, a_, s_) {
 		this.acc.add(f);
 	}
 
-	this.repel = function(e) {
+	// Purse red objects.
+	this.purse = function(tgt) {
+		// First get distance and calculate future location.
+		d = p5.Vector.sub(tgt.loc, this.loc).mag();
+		future_vel = tgt.vel.mult(0.001*d);
+		future_loc = tgt.loc.add(future_vel);
+		// Calculate desired velocity.
+		desired = p5.Vector.sub(future_loc, this.loc);
+		spd = map(d, 1, 10, this.mxvel, 0);
+		desired.setMag(spd);
+		// Apply steering force.
+		steer = p5.Vector.sub(desired, this.vel);
+		this.applyForce(steer);
+	}
+
+	// Evade blue objects.
+	this.evade = function(tgt) {
+		// First get distance and calculate future location.
+		d = p5.Vector.sub(tgt.loc, this.loc).mag();
+		future_vel = tgt.vel.mult(0.001*d);
+		future_loc = tgt.loc.add(future_vel);
+		// Calculate desired velocity.
+		desired = p5.Vector.sub(future_loc, this.loc);
+		spd = map(d, 1, 20, this.mxvel, 0);
+		desired.setMag(spd);
+		// Apply steering force.
+		steer = p5.Vector.sub(desired, this.vel);
+		steer.mult(-1);
+		this.applyForce(steer);
+	}
+
+	/*this.repel = function(e) {
 		var f = p5.Vector.sub(this.loc, e.loc);
 		var dist = f.mag();
 		f.normalize();
@@ -99,7 +162,7 @@ function Entity(x_, y_, p_, a_, s_) {
 		}
 		f.mult(strength);
 		this.applyForce(f);
-	}
+	}*/
 
 	this.bounce = function() {
 		var f = createVector(0,0)
@@ -107,6 +170,7 @@ function Entity(x_, y_, p_, a_, s_) {
 		if(this.loc.x  + this.rds >= windowWidth) f.add(createVector(-5,0));
 		if(this.loc.y  + this.rds <= 0) f.add(createVector(0,5));
 		if(this.loc.y  + this.rds >= windowHeight) f.add(createVector(0,-5));
+		this.acc.mult(0);
 		this.applyForce(f);
 	}
 
