0adebad04b25eecc31d5ef4a80d18953b4eb730b
Manuel Rueda <manolorueda@gmail.com>
2017-02-08 19:50:49 -0500

    pixel_mirror complete

--- pixelated/sketch.js
+++ pixelated/sketch.js
@@ -8,14 +8,15 @@ function setup() {
   // Mode and canvas.
   var canvas = createCanvas(w, h);
   canvas.parent('sketch-holder');
+  canvas.position(windowWidth/2 - w/2, 150);
   pixelDensity(1);
   // Capture photo.
   capture = createCapture(VIDEO);
   capture.size(round(w/f), round(h/f));
   capture.hide();
   // Create circles.
-  for(var x=0; x<w/f; x++) {
-    for(var y=0; y<h/f; y++) {
+  for(var x=1; x<w/f; x++) {
+    for(var y=1; y<h/f; y++) {
       circles.push(new Circle(x, y));
     }
   }
