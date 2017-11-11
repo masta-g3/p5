567d36b6e12cff0f7605b9467d93c1110068a41a
Manuel Rueda <manolorueda@gmail.com>
2017-11-10 22:45:55 -0800

    recursion

--- recursion/sketch.js
+++ recursion/sketch.js
@@ -11,7 +11,7 @@ function setup() {
 
 function draw() {
 	background(246);
-	tint(255, 200);
+	tint(255, 160);
 	image(img, width*0.6, height*0.4);
 	rec_square(width/2, height/2, width, toff);
 	toff += 0.01;
