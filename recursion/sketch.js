a2878ea9a31ea593df0cae1051ac547f47741bfa
Manuel Rueda <manolorueda@gmail.com>
2017-11-10 22:47:39 -0800

    recursion

--- recursion/sketch.js
+++ recursion/sketch.js
@@ -12,7 +12,7 @@ function setup() {
 function draw() {
 	background(246);
 	tint(255, 160);
-	image(img, width*0.6, height*0.4);
+	image(img, width*0.6 + shift1, height*0.4 + shift2);
 	rec_square(width/2, height/2, width, toff);
 	toff += 0.01;
 	// Trigger random size.
